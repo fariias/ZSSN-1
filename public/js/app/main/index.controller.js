6f4717a5c40146ca1b29a2aa894aad9a05dfe4ac
Leonardo Freitas dos Santos <leonardo_freitas1995@hotmail.com>
2016-12-21 12:24:32 -0200

    Creating home page

--- public/js/app/main/index.controller.js
+++ public/js/app/main/index.controller.js
@@ -6,5 +6,32 @@
     function Controller() {
         var vm = this;
 
+        vm.homeLinks = [
+            {
+                icon: "perm_identity",
+                title: "main.newSurvivor",
+                href: "/new_survivor"
+            },
+            {
+                icon: "edit",
+                title: "main.updateSurvivor",
+                href: "/update_survivor"
+            },
+            {
+                icon: "contacts",
+                title: "main.findSomeone",
+                href: "/find_survivor"
+            },
+            {
+                icon: "explore",
+                title: "main.map",
+                href: "/map"
+            },
+            {
+                icon: "show_chart",
+                title: "main.statistic",
+                href: "/statistic"
+            }
+        ];
     }
 })();
--- public/js/app/translate/translate.ptbr.js
+++ public/js/app/translate/translate.ptbr.js
@@ -3,7 +3,15 @@
         .module('zssn')
         .constant('translatePTBR', {
             main: {
-                zssn: "Zombie Survival Social Network"
+                zssn: "ZSSN",
+                zssnExt: "Zombie Survival Social Network",
+                welcome: "Bem vindo ao Zombie Survival Social Network",
+                lookingFor: "O que você está procurando?",
+                newSurvivor: "Sou novo aqui",
+                updateSurvivor: "Quero atualizar meus dados",
+                findSomeone: "Quero procurar alguém",
+                statistic: "Quero ver as últimas estatísticas",
+                map: "Quero ver o mapa de sobreviventes"
             }
         });
 })();
